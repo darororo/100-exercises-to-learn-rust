4c7ee1f0839d69ae5cf4da25903da2b644c2ce44
Yem Daro <100412991+darororo@users.noreply.github.com>
2024-08-24 14:16:40 +0700

    03_12 solution

--- exercises/03_ticket_v1/12_outro/src/lib.rs
+++ exercises/03_ticket_v1/12_outro/src/lib.rs
@@ -11,3 +11,79 @@
 // Integration here has a very specific meaning: they test **the public API** of your project.
 // You'll need to pay attention to the visibility of your types and methods; integration
 // tests can't access private or `pub(crate)` items.
+
+use std::iter::Product;
+
+pub struct Order {
+    product_name: String,
+    quantity: u32,
+    unit_price: u32,
+}
+
+impl Order {
+    pub fn new(product_name: String, quantity: u32, unit_price: u32) -> Self {
+        if product_name.is_empty() {
+            panic!()
+        }
+
+        if product_name.capacity() > 300 {
+            panic!()
+        }
+
+        if quantity <= 0 {
+            panic!()
+        }
+
+        if unit_price <= 0 {
+            panic!()
+        }
+
+        Self {
+            product_name,
+            quantity,
+            unit_price,
+        }
+    }
+
+    pub fn product_name(&self) -> &String {
+        &self.product_name
+    }
+
+    pub fn quantity(&self) -> &u32 {
+        &self.quantity
+    }
+
+    pub fn unit_price(&self) -> &u32 {
+        &self.unit_price
+    }
+
+    pub fn total(&self) -> u32 {
+        self.quantity * self.unit_price
+    }
+
+    pub fn set_product_name(&mut self, new_product_name: String) {
+        if new_product_name.is_empty() {
+            panic!()
+        }
+
+        if new_product_name.capacity() > 300 {
+            panic!()
+        }
+
+        self.product_name = new_product_name;
+    }
+
+    pub fn set_quantity(&mut self, new_quantity: u32) {
+        if new_quantity <= 0 {
+            panic!()
+        }
+        self.quantity = new_quantity;
+    }
+
+    pub fn set_unit_price(&mut self, new_unit_price: u32) {
+        if new_unit_price <= 0 {
+            panic!()
+        }
+        self.unit_price = new_unit_price;
+    }
+}

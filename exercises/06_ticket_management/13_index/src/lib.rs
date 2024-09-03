dccd5b5778f9415394415a3e9fa5e282de659e01
Yem Daro <100412991+darororo@users.noreply.github.com>
2024-09-03 23:28:36 +0700

    08_13 solution

--- exercises/06_ticket_management/13_index/src/lib.rs
+++ exercises/06_ticket_management/13_index/src/lib.rs
@@ -1,6 +1,7 @@
 // TODO: Implement `Index<&TicketId>` and `Index<TicketId>` for `TicketStore`.
 
 use ticket_fields::{TicketDescription, TicketTitle};
+use std::ops::Index;
 
 #[derive(Clone)]
 pub struct TicketStore {
@@ -55,6 +56,20 @@ impl TicketStore {
 
     pub fn get(&self, id: TicketId) -> Option<&Ticket> {
         self.tickets.iter().find(|&t| t.id == id)
+    }
+}
+
+impl Index<TicketId> for TicketStore {
+    type Output = Ticket;
+    fn index(&self, index: TicketId) -> &Self::Output {
+        self.tickets.iter().find(|x| x.id == index ).unwrap()
+    }
+}
+
+impl Index<&TicketId> for TicketStore {
+    type Output = Ticket;
+    fn index(&self, index: &TicketId) -> &Self::Output {
+        self.tickets.iter().find(|x| x.id == *index ).unwrap()
     }
 }
 

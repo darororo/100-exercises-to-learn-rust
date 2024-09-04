2abaf77933ec58fcbc0f579c052fd519cfe09d11
Yem Daro <100412991+darororo@users.noreply.github.com>
2024-09-04 15:10:41 +0700

    07_02 solution

--- exercises/07_threads/02_static/src/lib.rs
+++ exercises/07_threads/02_static/src/lib.rs
@@ -4,7 +4,13 @@
 use std::thread;
 
 pub fn sum(slice: &'static [i32]) -> i32 {
-    todo!()
+    let mid = slice.len()/2;
+    let left = &slice[..mid];
+    let right = &slice[mid..];
+    let sum1 = thread::spawn(|| left.iter().sum::<i32>());
+    let sum2 = thread::spawn(|| right.iter().sum::<i32>());
+
+    sum1.join().unwrap() + sum2.join().unwrap()
 }
 
 #[cfg(test)]
